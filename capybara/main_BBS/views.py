92b9720b00817be57de4428118077b5ee6fb4a88
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-07-15 16:55:08 +0900

    新しくdjangoのファイルを作って、環境構築をしました

--- capybara/main_BBS/views.py
+++ capybara/main_BBS/views.py
@@ -1,7 +1,3 @@
 from django.shortcuts import render
-from django.views.generic import ListView
-from . import models
+
 # Create your views here.
-class mainBBSview(ListView):
-    template_name=""
-    model = models.modelclassname
