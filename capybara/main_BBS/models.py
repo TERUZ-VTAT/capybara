7fc2082180b52c44e1ad5f4cecef32e8cff469bc
TERUZ-VTAT <terunori.kato0911@gmail.com>
2023-08-03 10:44:13 +0900

    update something

--- capybara/main_BBS/models.py
+++ capybara/main_BBS/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class UserAccount(models.Model):
+    username = models.CharField(max_length=25)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.username}"
--- capybara/main_BBS/views.py
+++ capybara/main_BBS/views.py
@@ -3,5 +3,5 @@ from django.views.generic import ListView
 from . import models
 # Create your views here.
 class mainBBSview(ListView):
-    template_name=""
-    model = models.modelclassname
+    template_name="main_bbs.html"
+    model = models.UserAccount
