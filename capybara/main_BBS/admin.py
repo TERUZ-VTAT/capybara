7c7d7570e1a2c7185031848f5b6d99741b7a56d5
TERUZ-VTAT <130330490+TERUZ-VTAT@users.noreply.github.com>
2023-08-29 12:00:47 +0900

    create group select page pack

--- capybara/main_BBS/admin.py
+++ capybara/main_BBS/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-admin.site.register(models.UserAccount)
+admin.site.register(models.Rooms)
--- capybara/main_BBS/views.py
+++ capybara/main_BBS/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from . import models
 # Create your views here.
-class mainBBSview(ListView):
-    template_name="main_bbs.html"
-    model = models.UserAccount
+class GroupSelect(ListView):
+    template_name="groupselect.html"
+    model = models.Rooms
