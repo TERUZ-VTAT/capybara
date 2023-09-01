e08f284c05edbd0fbce3a402cdd6b33e3dee39cc
Hauto Norimatsu <130329392+haut-norima@users.noreply.github.com>
2023-09-01 16:13:30 +0900

    ログイン画面のスタイル、サインアップ時のエラーの表示を追加

--- www/capybara_nori/capybara_nori/settings.py
+++ www/capybara_nori/capybara_nori/settings.py
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
