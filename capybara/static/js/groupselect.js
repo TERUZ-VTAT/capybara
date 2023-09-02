c427215161db1769bfe47a2d96b65d831cb65243
shishido-takuma <130330524+takuma-shishido@users.noreply.github.com>
2023-09-03 08:40:09 +0900

    groupselect.js refactor

--- capybara/static/js/groupselect.js
+++ capybara/static/js/groupselect.js
@@ -1,56 +1,50 @@
-var groupCircles = document.getElementsByClassName("groupcircle");
-var groupFrames = document.getElementsByClassName("groupsframe");
-var roommembers = document.getElementsByClassName("roommember")
+const groupCircles = document.getElementsByClassName("groupcircle");
+const groupFrames = document.getElementsByClassName("groupsframe");
+const roomMembers = document.getElementsByClassName("roommember");
 
-for ( var i=0; i < groupCircles.length; i++ ) {
+const sideMenu = document.getElementById("sidemenu");
 
-    var groupCircle = groupCircles[i];
-    var frame = groupFrames[i];
-    var membercount = parseInt(roommembers[i].textContent.split(":")[1]);
+for ( const i = 0; i < groupCircles.length; i++ ) {
 
-    var frameWidth = frame.clientWidth;
-    var frameHeight = frame.clientHeight;
+    const groupCircle = groupCircles[i];
+    const frame = groupFrames[i];
+    const memberCount = parseInt(roomMembers[i].textContent.split(":")[1]);
 
-    var circlesize = returncirclesize(membercount);
+    const frameWidth = frame.clientWidth;
+    const frameHeight = frame.clientHeight;
+    
+    const circleSize = getCircleSize(memberCount);
 
-    randomTop = getRandomNumber(frameHeight, circlesize);
-    randomLeft = getRandomNumber(frameWidth, circlesize);
+    randomTop = getRandomNumber(frameHeight, circleSize);
+    randomLeft = getRandomNumber(frameWidth, circleSize);
 
     groupCircle.style.top = randomTop +"px";
     groupCircle.style.left = randomLeft +"px";
-    groupCircle.style.height = circlesize + "px";
-    groupCircle.style.width = circlesize + "px";
+    groupCircle.style.height = circleSize + "px";
+    groupCircle.style.width = circleSize + "px";
 
-    groupCircle.setAttribute("href", location.href+"rooms/"+groupCircle.id+"/")
+    groupCircle.setAttribute("href", location.href + "rooms/" + groupCircle.id + "/");
 
-    // console.log({{ roomdata.roomMember_Instant }}/100);
-
+    // console.log({{ roomdata.roomMember_Instant }} / 100);
 }
 
-function returncirclesize(memberCounter) {
-    if ( memberCounter > 500) {
-        return 400;
-    } else {
-        return 150 + Math.floor(memberCounter/2);
-    }
+function getCircleSize(memberCount) {
+    if ( memberCount > 500) return 400;
+     else return 150 + Math.floor(memberCount / 2);
 }
 
 function getRandomNumber(max, size) {
-
     return Math.random() * (max - size);
-
 }
 
-var sidemenu = document.getElementById("sidemenu");
-
-function sidemenuopen() {
-    for ( var i=0; i<10; i++) {
-        sidemenu.style.left = 0+"px";
+function sideMenuOpen() {
+    for ( const i = 0; i < 10; i++) {
+        sideMenu.style.left = 0 + "px";
     }
 }
 
-function sidemenuclose() {
-    for ( var i=0; i<10; i++) {
-        sidemenu.style.left = ((i+1)*-34)+"px";
+function sideMenuClose() {
+    for ( const i = 0; i < 10; i++) {
+        sideMenu.style.left = ((i+1) * -34) + "px";
     }
 }
