e8587602deb7bc102d472f8b5e4acbcea93b4a25
shishido-takuma <130330524+takuma-shishido@users.noreply.github.com>
2023-09-06 16:30:17 +0900

    animation

--- www/capybara_nori/static/js/groupselect.js
+++ www/capybara_nori/static/js/groupselect.js
@@ -4,47 +4,49 @@ const roomMembers = document.getElementsByClassName("roommember");
 
 const sideMenu = document.getElementById("sidemenu");
 
-for ( let i = 0; i < groupCircles.length; i++ ) {
+for (let i = 0; i < groupCircles.length; i++) {
+  const groupCircle = groupCircles[i];
+  const frame = groupFrames[i];
+  const memberCount = parseInt(roomMembers[i].textContent.split(":")[1]);
 
-    const groupCircle = groupCircles[i];
-    const frame = groupFrames[i];
-    const memberCount = parseInt(roomMembers[i].textContent.split(":")[1]);
+  const frameWidth = frame.clientWidth;
+  const frameHeight = frame.clientHeight;
 
-    const frameWidth = frame.clientWidth;
-    const frameHeight = frame.clientHeight;
+  const circleSize = getCircleSize(memberCount);
 
-    const circleSize = getCircleSize(memberCount);
+  randomTop = getRandomNumber(frameHeight, circleSize);
+  randomLeft = getRandomNumber(frameWidth, circleSize);
 
-    randomTop = getRandomNumber(frameHeight, circleSize);
-    randomLeft = getRandomNumber(frameWidth, circleSize);
+  groupCircle.style.top = randomTop + "px";
+  groupCircle.style.left = randomLeft + "px";
+  groupCircle.style.height = circleSize + "px";
+  groupCircle.style.width = circleSize + "px";
 
-    groupCircle.style.top = randomTop +"px";
-    groupCircle.style.left = randomLeft +"px";
-    groupCircle.style.height = circleSize + "px";
-    groupCircle.style.width = circleSize + "px";
+  groupCircle.setAttribute(
+    "href",
+    location.href + "rooms/" + groupCircle.id + "/"
+  );
 
-    groupCircle.setAttribute("href", location.href + "rooms/" + groupCircle.id + "/");
-
-    // console.log({{ roomdata.roomMember_Instant }} / 100);
+  // console.log({{ roomdata.roomMember_Instant }} / 100);
 }
 
 function getCircleSize(memberCount) {
-    if ( memberCount > 500) return 400;
-    else return 150 + Math.floor(memberCount / 2);
+  if (memberCount > 500) return 400;
+  else return 150 + Math.floor(memberCount / 2);
 }
 
 function getRandomNumber(max, size) {
-    return Math.random() * (max - size);
+  return Math.random() * (max - size);
 }
 
 function sideMenuOpen() {
-    for ( let i = 0; i < 10; i++) {
-        sideMenu.style.left = 0 + "px";
-    }
+  $("#sidemenu").animate({
+    marginLeft: "340px",
+  });
 }
 
 function sideMenuClose() {
-    for ( let i = 0; i < 10; i++) {
-        sideMenu.style.left = ((i+1) * -340) + "px";
-    }
+  $("#sidemenu").animate({
+    marginLeft: "0px",
+  });
 }
