a1bf969495c6979ce7c87f2ec22f43e4aec71736
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-09-07 00:31:43 +0900

    created Chat models and connected the models into the views.py

--- capybara_chat/views.py
+++ capybara_chat/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Chat # Chat DB をインストール
 
 
 def lobby(request):
-    return render(request, 'chat/lobby.html') #chat/lobby.htmlのパスが追加されている
+    if request.POST:
+        message = request.POST["message"] # lobby.htmlに書いてあるformタグの中にあるmessageを変数messageに格納する
+        chat_obj = Chat(message=message)
+        chat_obj.save()
+
+    all_messages = Chat.objects.all()
+
+    return render(request, 'chat/lobby.html', {'all_messages': all_messages}) #chat/lobby.htmlのパスが追加されている
