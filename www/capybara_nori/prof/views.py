18a36fbbd7a194148d624431808c16ec7751fcb9
shishido-takuma <130330524+takuma-shishido@users.noreply.github.com>
2023-09-06 11:22:04 +0900

    v1

--- www/capybara_nori/prof/views.py
+++ www/capybara_nori/prof/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import ListView,TemplateView,DetailView
+from django.urls import reverse_lazy
 from . import models
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
-class profileview(DetailView):
+
+class profileview(LoginRequiredMixin, DetailView):
     model=models.profile
+    login_url="/"
     template_name="profile.html"
