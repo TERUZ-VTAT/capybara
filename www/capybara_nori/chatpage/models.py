649c7da17ce6d00606658e95f4d244455bfc8cdd
TERUZ-VTAT <130330490+TERUZ-VTAT@users.noreply.github.com>
2023-09-11 01:33:19 +0900

    add room id column

--- www/capybara_nori/chatpage/models.py
+++ www/capybara_nori/chatpage/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class Chat(models.Model):
     message = models.TextField(max_length=300)
     created_at = models.DateTimeField(auto_now=True)
+    sent_room_id = models.IntegerField()
     # user = models.ManyToManyField(userのDB名)
