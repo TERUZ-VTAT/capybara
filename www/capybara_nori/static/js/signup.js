199473fd440585259e6dbc91337f84dd0773bbe0
shishido-takuma <130330524+takuma-shishido@users.noreply.github.com>
2023-09-11 12:54:24 +0900

    login.js signup.js refactor

--- www/capybara_nori/static/js/signup.js
+++ www/capybara_nori/static/js/signup.js
@@ -1,9 +1,7 @@
+window.onload = function () {
+  const elem1 = document.getElementById("id_password1");
+  elem1.placeholder = "パスワードを入力してください";
 
-window.onload=function(){
-    let elem1 = document.getElementById('id_password1');
-    elem1.placeholder='パスワードを入力してください';
-
-    let elem2 = document.getElementById('id_password2');
-    elem2.placeholder='パスワードを再度入力してください';
-
-}
+  const elem2 = document.getElementById("id_password2");
+  elem2.placeholder = "パスワードを再度入力してください";
+};
